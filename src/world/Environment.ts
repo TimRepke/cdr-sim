cc06736984647c7cdf0c565bd4fb512bae4f5d2a
Tim Repke <repke@mcc-berlin.net>
2023-05-30 20:11:59 +0200

    some more helper utils

--- src/world/Environment.ts
+++ src/world/Environment.ts
@@ -6,7 +6,7 @@ import { idx2ym } from './util';
 export class Environment {
   private stepIdx: number;
 
-  private actors: Record<ActorKey, Actor>;
+  public actors: Record<ActorKey, Actor>;
 
   private state: Record<ResourceKey, StateVariable>;
 
@@ -36,23 +36,53 @@ export class Environment {
       if (emits) {
         Object.entries(emits).forEach((entry) => {
           const [key, amounts] = entry;
-          this.state[key as ResourceKey].ledger[this.stepIdx].emitted += amounts[this.stepIdx] * (factor as number);
+          this.state[key as ResourceKey].logEmission(this.stepIdx, amounts[this.stepIdx] * (factor as number));
         });
       }
       if (consumes) {
         Object.entries(consumes).forEach((entry) => {
           const [key, amounts] = entry;
-          this.state[key as ResourceKey].ledger[this.stepIdx].consumed += amounts[this.stepIdx] * (factor as number);
+          this.state[key as ResourceKey].logConsumption(this.stepIdx, amounts[this.stepIdx] * (factor as number));
         });
       }
       if (occupies) {
         Object.entries(occupies).forEach((entry) => {
           const [key, amounts] = entry;
-          this.state[key as ResourceKey].ledger[this.stepIdx].occupied += amounts[this.stepIdx] * (factor as number);
+          this.state[key as ResourceKey].logOccupation(this.stepIdx, amounts[this.stepIdx] * (factor as number));
         });
       }
     });
 
     this.stepIdx += 1;
   }
+
+  logSpecial(event: SpecialEvent) {
+    const { consumes, emits, occupies } = event;
+    if (emits) {
+      Object.entries(emits).forEach((entry) => {
+        const [key, amount] = entry;
+        this.state[key as ResourceKey].logEmission(this.stepIdx, amount);
+      });
+    }
+    if (consumes) {
+      Object.entries(consumes).forEach((entry) => {
+        const [key, amount] = entry;
+        this.state[key as ResourceKey].logEmission(this.stepIdx, amount);
+      });
+    }
+    if (occupies) {
+      Object.entries(occupies).forEach((entry) => {
+        const [key, amount] = entry;
+        this.state[key as ResourceKey].logOccupation(this.stepIdx, amount);
+      });
+    }
+  }
+}
+
+export interface SpecialEvent {
+  name: string;
+  description: string;
+  consumes?: Partial<Record<ResourceKey, number>>;
+  emits?: Partial<Record<ResourceKey, number>>;
+  occupies?: Partial<Record<ResourceKey, number>>;
 }
--- src/world/State.ts
+++ src/world/State.ts
@@ -6,43 +6,81 @@ export type ResourceKey =
   | 'BUDGET';
 
 export interface LedgerEntry {
-  net: number;
   consumed: number;
   emitted: number;
   occupied: number;
+  net: number;
 }
 
-export interface StateVariable {
-  name: I18nString,
-  ledger: Array<LedgerEntry>,
-  offset: number,
-  isConstant?: boolean,
+export interface StateVariableStart {
+  name: I18nString;
+  offset: number;
+  isConstant?: boolean;
 }
 
-const StateStart: Record<ResourceKey, StateVariable> = {
+export type StateType = Record<ResourceKey, StateVariable>;
+
+const StateStart: Record<ResourceKey, StateVariableStart> = {
   CO2_ATMOSPHERE: {
     name: {
       EN: 'CO2 in the atmosphere',
     },
     offset: 41340000000, // ~41Gt emitted by 2000 https://github.com/owid/co2-data
-    ledger: [],
   },
   BUDGET: {
     name: {
       EN: 'Budget from CO2 pricing',
     },
     offset: 0,
-    ledger: [],
   },
 };
+
+export class StateVariable implements StateVariableStart {
+  public name: I18nString;
+
+  public offset: number;
+
+  public isConstant: boolean;
+
+  public ledger: Array<LedgerEntry>;
+
+  constructor(startState: StateVariableStart) {
+    this.name = startState.name;
+    this.offset = startState.offset;
+    this.isConstant = !!startState.isConstant;
+    this.ledger = [...Array(((YEAR_LAST - YEAR_FIRST) + 1) * 12)].map(() => ({ net: 0, consumed: 0, emitted: 0, occupied: 0 }));
+  }
+
+  getNetAt(idx: number): number | undefined {
+    if (idx >= this.ledger.length || idx < 0) return undefined;
+    return this.ledger[idx].emitted - this.ledger[idx].consumed;
+  }
+
+  getCumAt(idx: number): number | undefined {
+    if (idx >= this.ledger.length || idx < 0) return undefined;
+    return this.offset + [...Array(idx)].map((i) => this.getNetAt(i) as number).reduce((acc, v) => acc + v);
+  }
+
+  logEmission(step: number, amount: number) {
+    this.ledger[step].emitted += amount;
+    this.ledger[step].net += amount;
+  }
+
+  logConsumption(step: number, amount: number) {
+    this.ledger[step].consumed += amount;
+    this.ledger[step].net -= amount;
+  }
+
+  logOccupation(step: number, amount: number) {
+    this.ledger[step].occupied += amount;
+  }
+}
 
 export function getFreshState(): Record<ResourceKey, StateVariable> {
   return Object.fromEntries(
     Object.entries(StateStart).map((entry) => {
       const [key, variable] = entry;
-      variable.ledger = [...Array(((YEAR_LAST - YEAR_FIRST) + 1) * 12)]
-        .map(() => ({ net: 0, consumed: 0, emitted: 0, occupied: 0 }));
-      return [key as ResourceKey, variable];
+      return [key as ResourceKey, new StateVariable(variable)];
     }),
   ) as Record<ResourceKey, StateVariable>;
 }
